9c6b27e2aceaf161a7021bd96c183084128dab7c
itWelt <rassulovmurad@gmail.com>
2023-03-15 21:20:26 +0300

    [main][task] create prototype of pages/signup

---
+++ pages/signup/index.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+.signup signup
+</template>
+
+<script src="./signup.js" />
+<style lang="scss" src="./signup.scss" />
---
+++ pages/signup/signup.js
@@ -0,0 +1,23 @@
+export default {
+  components: {},
+
+  props: {},
+  data: () => ({}),
+  computed: {},
+
+  watch: {},
+  methods: {
+    /* GETTERS */
+    /* SETTERS */
+    /* HANDLERS */
+    /* HELPERS */
+    /* ACTIONS */
+  },
+
+  created() {
+    console.debug("pages/signup/created"); //DELETE
+  },
+  mounted() {
+    console.debug("pages/signup/mounted"); //DELETE
+  },
+}
