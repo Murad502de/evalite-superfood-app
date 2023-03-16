03d2915c06b20dd7ec17a394615fe6330cd6637b
itWelt <rassulovmurad@gmail.com>
2023-03-16 15:07:59 +0300

    [main][task] implement layout of pages/forgot/ConfirmCard

--- pages/forgot/components/ConfirmCard/index.vue
+++ pages/forgot/components/ConfirmCard/index.vue
@@ -1,24 +1,26 @@
 <template lang="pug">
 v-card.forgot-confirm-card(elevation="3", outlined, width="360")
-  v-card-subtitle.forgot-confirm-card__title Введите код из E-mail
+  v-card-title.forgot-confirm-card__title Введите код из E-mail
   v-form.forgot-confirm-card__form(
     ref="form",
     v-model="valid",
     lazy-validation
   )
-    v-otp-input(length="4", type="number")
+    v-card-subtitle.forgot-confirm-card__info Письмо с кодом отправлено на test@gmail.com
 
-    v-btn.forgot-confirm-card__btn(
+    v-otp-input.forgot-confirm-card__code-input(length="4", type="number")
+
+    v-btn.forgot-confirm-card__btn.forgot-confirm-card__btn--confirm(
       :disabled="!valid && !loading",
       :loading="loading",
       color="#0082DE",
     ) отправить код
 
-    v-btn.forgot-confirm-card__btn(
+    v-btn.forgot-confirm-card__btn.forgot-confirm-card__btn--send-code(
       outlined
-      :disabled="!valid && !loading",
+      :disabled="!loading",
       :loading="loading",
-    ) Отправить повторно через 30 сек
+    ) отправить повторно через 30 сек
 </template>
 
 <script src="./confirmCard.js" />
