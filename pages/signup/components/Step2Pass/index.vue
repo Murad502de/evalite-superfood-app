29fca846471fbd3e410fdb67a02b61a7f852e125
itWelt <rassulovmurad@gmail.com>
2023-03-22 15:17:04 +0300

    [main][task] fixation commit for signup step3

--- pages/signup/components/Step2Pass/index.vue
+++ pages/signup/components/Step2Pass/index.vue
@@ -1,6 +1,41 @@
 <template lang="pug">
 .signup-step1-pass
   .signup-step1-pass__title Введите пароль
+  v-form.signup-step1-pass__form(ref="form", v-model="valid", lazy-validation)
+    v-text-field(
+      v-model="password",
+      :type="showPass ? 'text' : 'password'",
+      :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'",
+      @click:append="showPass = !showPass",
+      :rules="passwordRules",
+      :disabled="loading",
+      label="Введите новый пароль",
+      required,
+      outlined
+    )
+
+    v-text-field(
+      v-model="password1",
+      :type="showPass1 ? 'text' : 'password'",
+      :append-icon="showPass1 ? 'mdi-eye' : 'mdi-eye-off'",
+      @click:append="showPass1 = !showPass1",
+      :rules="passwordRules",
+      :disabled="loading",
+      label="Введите новый пароль повторно",
+      required,
+      outlined
+    )
+
+    .signup-step1-pass__info
+      span Требования к паролю:
+      br
+      span длина — не менее 8 символов;
+      br
+      span заглавные буквы;
+      br
+      span строчные буквы;
+      br
+      span цифры или специальные символы: %, #, $ и другие.
 </template>
 
 <script src="./Step2Pass.js" />
--- pages/signup/signup.js
+++ pages/signup/signup.js
@@ -21,7 +21,7 @@ export default {
   props: {},
   data() {
     return {
-      onboarding: 1,
+      onboarding: 2,
       formsCount: 5,
       confirmTimerCount: 30,
       confirmTimer: null,
