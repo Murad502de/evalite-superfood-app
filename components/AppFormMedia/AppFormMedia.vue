e715e54b54ae86ea979df12163ed0fd7e22af673
itWelt <rassulovmurad@gmail.com>
2023-04-10 20:46:35 +0300

    [main][task] styled AppFormMedia

--- components/AppFormMedia/AppFormMedia.vue
+++ components/AppFormMedia/AppFormMedia.vue
@@ -11,13 +11,13 @@
     )
 
     .app-form-media-file
-      .app-form-media-file__name Образец
+      .app-form-media-file__name {{ mediaName || "Образец" }}
 
-      .app-form-media-file__stub
+      .app-form-media-file__media(v-show="!mediaUrl")
         slot(name="stub-img")
 
-      .app-form-media-file__stub
-        img(:src="uploadedFileUrl")
+      .app-form-media-file__media(v-show="!!mediaUrl")
+        img.app-form-media-file__media--img(:src="mediaUrl")
 
       .app-form-media-file__delete(v-show="!!mediaUrl", @click="deleteFile")
         TrashIconSvg.app-form-media-file__delete-icon
