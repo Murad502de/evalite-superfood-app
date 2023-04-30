b449bdc3cb79b621262b3168049aab6073b4d916
itWelt <rassulovmurad@gmail.com>
2023-04-30 13:54:28 +0300

    [main][task] implemented api/users/usersPasswordUpdate

---
+++ api/users/usersPasswordUpdate.js
@@ -0,0 +1,23 @@
+import { api } from '@/api';
+
+export const usersPasswordUpdate = async (email = null, confirm_code = null, password = null) => {
+  if (!email || !confirm_code || !password) return;
+
+  const payload = {
+    email,
+    confirm_code,
+    password,
+  };
+
+  console.debug('api/users/usersPasswordUpdate.js/usersPasswordUpdate/payload', payload); //DELETE
+
+  try {
+    const response = await api.put('users/password', payload);
+
+    console.debug('api/users/usersPasswordUpdate.js/usersPasswordUpdate/response', response); //DELETE
+
+    return response;
+  } catch (e) {
+    return Object.assign({}, e).response;
+  }
+};
