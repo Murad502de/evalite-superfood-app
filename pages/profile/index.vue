cffe8855b671154fef8362e30797700b444b4b09
itWelt <rassulovmurad@gmail.com>
2023-05-05 18:21:33 +0300

    [main][task] added empty profile page

---
+++ pages/profile/index.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+.ruqi-applications ruqi-profile
+</template>
+
+<script>
+export default {
+  layout: "default",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+  created() {},
+  mounted() {},
+};
+</script>
+
+<style lang="scss">
+</style>
