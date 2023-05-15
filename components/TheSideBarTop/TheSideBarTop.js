9cbb1ab86ee4b0c6950c5e5602c255042c0fc56b
itWelt <rassulovmurad@gmail.com>
2023-05-15 15:21:47 +0300

    [main][task] implemented TheSideBarTop prototype

---
+++ components/TheSideBarTop/TheSideBarTop.js
@@ -0,0 +1,21 @@
+export default {
+  components: {},
+
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+
+  watch: {},
+  methods: {
+    /* GETTERS */
+    /* SETTERS */
+    /* HANDLERS */
+    /* HELPERS */
+    /* ACTIONS */
+  },
+
+  created() {},
+  mounted() {},
+}
---
+++ components/TheSideBarTop/TheSideBarTop.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+.the-side-bar-top TheSideBarTop
+</template>
+
+<script src="./TheSideBarTop.js" />
+<style lang="scss" src="./TheSideBarTop.scss" />
