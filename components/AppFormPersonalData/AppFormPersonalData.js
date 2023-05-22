329a4b117f9248b70a7d380e039172b3ac0772d1
itWelt <rassulovmurad@gmail.com>
2023-05-22 15:58:16 +0300

    [main][task] implemented AppFormPersonalData prototype

--- components/AppFormPersonalData/AppFormPersonalData.js
+++ components/AppFormPersonalData/AppFormPersonalData.js
@@ -64,7 +64,6 @@ export default {
   computed: {
     computedProgress() {
       let progress = 0;
-
       if (this.secondName.length) progress += 10;
       if (this.firstName.length) progress += 10;
       if (this.thirdName.length) progress += 10;
@@ -72,19 +71,55 @@ export default {
       if (this.birthday.length) progress += 10;
       if (this.email.length) progress += 10;
       if (this.phone.length) progress += 10;
-
       return progress;
     },
     formPlaceholder() {
       return formPlaceholder;
     },
   },
-  watch: {},
+  watch: {
+    computedProgress(newVal) {
+      console.debug('AppFormPersonalData/watch/computedProgress', newVal); //DELETE
+      this.$emit('update:progress', newVal);
+    },
+    avatarFile(newVal) {
+      console.debug('AppFormPersonalData/watch/avatarFile', newVal); //DELETE
+      this.$emit('update:avatar', newVal);
+    },
+    secondName(newVal) {
+      console.debug('AppFormPersonalData/watch/secondName', newVal); //DELETE
+      this.$emit('update:second_name', newVal);
+    },
+    firstName(newVal) {
+      console.debug('AppFormPersonalData/watch/firstName', newVal); //DELETE
+      this.$emit('update:first_name', newVal);
+    },
+    thirdName(newVal) {
+      console.debug('AppFormPersonalData/watch/thirdName', newVal); //DELETE
+      this.$emit('update:third_name', newVal);
+    },
+    gender(newVal) {
+      console.debug('AppFormPersonalData/watch/gender', newVal); //DELETE
+      const genderCode = newVal === 'муж' ? 'male' : 'female';
+      console.debug('AppFormPersonalData/watch/genderCode', genderCode); //DELETE
+      this.$emit('update:gender', genderCode);
+    },
+    birthday(newVal) {
+      console.debug('AppFormPersonalData/watch/birthday', newVal); //DELETE
+      this.$emit('update:birthday', newVal);
+    },
+    email(newVal) {
+      console.debug('AppFormPersonalData/watch/email', newVal); //DELETE
+      this.$emit('update:email', newVal);
+    },
+    phone(newVal) {
+      console.debug('AppFormPersonalData/watch/phone', newVal); //DELETE
+      this.$emit('update:phone', newVal);
+    },
+  },
   methods: {
     uploadAvatar(file = null) {
-      //TODO call validate service
       console.debug(file); //DELETE
-
       this.avatarFile = file;
       this.avatarName = file.name;
       this.avatarUrl = createUploadedFileUrl(file);
