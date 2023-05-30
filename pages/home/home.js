6450c21aa0cdb8449d806231e6f126b6ca5f31e4
itWelt <rassulovmurad@gmail.com>
2023-05-30 12:14:36 +0300

    [dev][task] edited allocator for home page by role

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,12 +1,9 @@
 import { mapGetters, mapActions, } from 'vuex';
 import * as roles from "@/shared/roles";
-// import HomeAdmin from './components/HomeAdmin/HomeAdmin.vue';
-import HomeAdmin from '@/pages/applications/index.vue';
 import HomeReferral from './components/HomeReferral/HomeReferral.vue';
 
 export default {
   components: {
-    HomeAdmin,
     HomeReferral,
   },
   props: {},
@@ -21,15 +18,17 @@ export default {
     ...mapActions('userStore', ['setUserData']),
     getComponent(name) {
       switch (name) {
-        case roles.admin:
-          return "HomeAdmin";
         case roles.referral:
           return "HomeReferral";
         default:
-          return "";
+          return null;
       }
     },
   },
-  created() {},
+  created() {
+    if (this.userData.role === roles.admin) {
+      this.$router.push('applications')
+    }
+  },
   mounted() { },
 };
