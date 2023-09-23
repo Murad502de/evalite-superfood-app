090fdc6e932591e460279db0802b8060dc89f6e2
itWelt <rassulovmurad@gmail.com>
2023-09-23 12:24:12 +0300

    [main][]

---
+++ pages/home/Home copy.js
@@ -0,0 +1,35 @@
+import { mapGetters, mapActions, } from 'vuex';
+import * as roles from "@/shared/roles";
+import HomePartner from './components/HomePartner/HomePartner.vue';
+
+export default {
+  components: {
+    HomePartner,
+  },
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters('userStore', ['userData']),
+  },
+  watch: {},
+  methods: {
+    ...mapActions('userStore', ['setUserData']),
+    getComponent(name) {
+      switch (name) {
+        case roles.referral:
+          return "HomePartner";
+        default:
+          return null;
+      }
+    },
+  },
+  created() {
+    if (this.userData.role === roles.admin) {
+      this.$router.push('applications')
+    }
+  },
+
+  mounted() { },
+};
