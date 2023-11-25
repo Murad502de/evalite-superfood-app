21a8e97cfb0087d3e02aea8f218c0f5dbf5fdd25
itWelt <rassulovmurad@gmail.com>
2023-11-25 17:28:05 +0300

    [main][]

--- components/PartnerProfile/PartnerProfile.vue
+++ components/PartnerProfile/PartnerProfile.vue
@@ -4,9 +4,7 @@
     AppCard.partner-profile-card
       PartnerProfileInfoCard
     .partner-profile-blocks
-      AppCard.partner-profile-block qwerty
-      AppCard.partner-profile-block qwerty
-      AppCard.partner-profile-block qwerty
+      AppCard.partner-profile-block(v-for="(i, index) in 5" :key="index") qwerty
 </template>
 
 <script src="./PartnerProfile.js" />
