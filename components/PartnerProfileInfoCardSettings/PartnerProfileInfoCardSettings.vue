ddb8d1482cfb2f18d8865a3fbb3622cda55cf1d4
itWelt <rassulovmurad@gmail.com>
2023-11-26 14:43:02 +0300

    [main][] implement PartnerProfileInfoCardSettings prototype

---
+++ components/PartnerProfileInfoCardSettings/PartnerProfileInfoCardSettings.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+.partner-profile-info-card-settings PartnerProfileInfoCardSettings
+</template>
+
+<script src="./PartnerProfileInfoCardSettings.js" />
+<style lang="scss" src="./PartnerProfileInfoCardSettings.scss" />
