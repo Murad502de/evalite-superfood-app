a90158dfb468ccfcece1f01c94122b1df3d32c9a
itWelt <rassulovmurad@gmail.com>
2023-11-29 02:27:16 +0300

    [main][]

---
+++ components/PartnerProfile/components/PersonalCardSettings/PersonalCardSettings.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+.personal-card-settings
+  AppCard.personal-card-settings--card
+    v-tabs.personal-card-settings--tabs(
+      v-model="tab",
+      :show-arrows="false",
+      center-active,
+      :hide-slider="disabled || saveLoading"
+    )
+      v-tab.personal-card-settings--tab(
+        :disabled="disabled || saveLoading"
+      ) Общее
+      v-tab.personal-card-settings--tab(
+        :disabled="disabled || saveLoading"
+      ) Пароль
+
+    v-window.personal-card-settings--window(v-model="tab")
+      v-window-item.personal-card-settings--window-item
+        AppFormPersonalData(
+          ref="partner_profile_personal_data_form",
+          :data="user",
+          :loading="loading",
+          :disabled="disabled || saveLoading",
+          @update:avatar="updateAvatar",
+          @update:second_name="updateSecondName",
+          @update:first_name="updateFirstName",
+          @update:third_name="updateThirdName",
+          @update:gender="updateGender",
+          @update:birthday="updateBirthday",
+          @update:email="updateEmail",
+          @update:phone="updatePhone"
+        )
+
+      v-window-item.personal-card-settings--window-item
+        AppFormPassword(
+          ref="partner_profile_personal_password_form",
+          @update:password="updatePassword"
+        )
+</template>
+
+<script src="./PersonalCardSettings.js" />
+<style lang="scss" src="./PersonalCardSettings.scss" />
