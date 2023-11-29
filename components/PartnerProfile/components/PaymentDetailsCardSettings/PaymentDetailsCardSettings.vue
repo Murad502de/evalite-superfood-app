467ca7dfaffc6efa491d50a1fcaaf506111eb24f
itWelt <rassulovmurad@gmail.com>
2023-11-29 03:11:13 +0300

    [main][] implemented components/PartnerProfile/components/payment-details prototype

---
+++ components/PartnerProfile/components/PaymentDetailsCardSettings/PaymentDetailsCardSettings.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+.payment-details-card-settings
+  AppCard.payment-details-card-settings--card PaymentDetailsCardSettings
+</template>
+
+<script src="./PaymentDetailsCardSettings.js" />
+<style lang="scss" src="./PaymentDetailsCardSettings.scss" />
