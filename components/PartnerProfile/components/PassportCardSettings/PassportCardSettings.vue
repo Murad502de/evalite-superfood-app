4bec58aae9aa5ed69cc27368747316082e4f71d7
itWelt <rassulovmurad@gmail.com>
2023-11-29 03:08:36 +0300

    [main][] implemented components/PartnerProfile/components/PassportCardSettings prototype

---
+++ components/PartnerProfile/components/PassportCardSettings/PassportCardSettings.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+.passport-card-settings
+  AppCard.passport-card-settings--card ContractCardSettings
+</template>
+
+<script src="./PassportCardSettings.js" />
+<style lang="scss" src="./PassportCardSettings.scss" />
