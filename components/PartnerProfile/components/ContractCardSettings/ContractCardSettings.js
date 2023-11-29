8586536de596ba34368884ae0eb2f17d00c45b20
itWelt <rassulovmurad@gmail.com>
2023-11-29 03:06:24 +0300

    [main][] implemented components/PartnerProfile/components/ContractCardSettings prototype

---
+++ components/PartnerProfile/components/ContractCardSettings/ContractCardSettings.js
@@ -0,0 +1,36 @@
+import AppCard from '@/components/AppCard/AppCard.vue';
+
+export default {
+  components: {
+    AppCard,
+  },
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    saveLoading: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      tab: 0,
+      valid: true,
+    };
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+  created() { },
+  mounted() { },
+};
---
+++ components/PartnerProfile/components/ContractCardSettings/ContractCardSettings.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+.contract-card-settings
+  AppCard.contract-card-settings--card ContractCardSettings
+</template>
+
+<script src="./ContractCardSettings.js" />
+<style lang="scss" src="./ContractCardSettings.scss" />
