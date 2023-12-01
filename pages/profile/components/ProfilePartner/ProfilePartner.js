f7ffbbb8101adee34f826cc21aed5f1f2674bcb4
itWelt <rassulovmurad@gmail.com>
2023-12-01 18:53:49 +0300

    [main][]

--- pages/profile/components/ProfilePartner/ProfilePartner.js
+++ pages/profile/components/ProfilePartner/ProfilePartner.js
@@ -1,4 +1,5 @@
 import { mapGetters, mapActions, } from 'vuex';
+import { createUploadedFileUrl } from '@/utils/file.js';
 import * as textContent from './shared/textContent';
 import PartnerProfile from '@/components/PartnerProfile/PartnerProfile.vue';
 
@@ -21,6 +22,11 @@ export default {
     ...mapActions('userStore', ['setUserData']),
     updateAvatar(value) {
       console.debug('ProfilePartner/updateAvatar', value); //DELETE
+      console.debug('ProfilePartner/updateAvatar/url', createUploadedFileUrl(value)); //DELETE
+      this.setUserData({
+        avatarFile: value,
+        avatar: createUploadedFileUrl(value),
+      });
     },
     updateSecondName(value) {
       console.debug('ProfilePartner/updateSecondName', value); //DELETE
@@ -40,15 +46,19 @@ export default {
     },
     updateBirthday(value) {
       console.debug('ProfilePartner/updateBirthday', value); //DELETE
+      this.setUserData({ birthday: value, });
     },
     updateEmail(value) {
       console.debug('ProfilePartner/updateEmail', value); //DELETE
+      this.setUserData({ email: value, });
     },
     updatePhone(value) {
       console.debug('ProfilePartner/updatePhone', value); //DELETE
+      this.setUserData({ phone: value, });
     },
     updatePassword(value) {
       console.debug('ProfilePartner/updatePassword', value); //DELETE
+      this.setUserData({ password: value, });
     },
   },
   created() { },
