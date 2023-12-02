ac949544994384dfd07d35103ef89174654feb0c
itWelt <rassulovmurad@gmail.com>
2023-12-02 13:05:29 +0300

    [main][]

--- components/PartnerProfile/shared/cardNames.js
+++ components/PartnerProfile/shared/cardNames.js
@@ -1,6 +1,6 @@
-export const contractCard = 'contractCard';
 export const personalCard = 'personalCard';
 export const passportCard = 'passportCard';
 export const paymentDetailsCard = 'paymentDetailsCard';
+export const contractCard = 'contractCard';
 
 export default null;
