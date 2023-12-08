8462d106f079b088d89a3a6ed7970b8b2dbca343
itWelt <rassulovmurad@gmail.com>
2023-12-08 14:11:50 +0300

    [main][] implemented confirm by saving in PartnerProfile

--- pages/profile/components/ProfilePartner/ProfilePartner.js
+++ pages/profile/components/ProfilePartner/ProfilePartner.js
@@ -19,17 +19,23 @@ export default {
   watch: {},
   methods: {
     ...mapActions('userStore', ['setUserData']),
+    confirm() {
+      return confirm('Применение данных настроек потребует повторной верификации');
+    },
     savePersonal(data) {
       console.debug('savePersonal/data', data); //DELETE
     },
     savePassport(data) {
       console.debug('savePassport/data', data); //DELETE
+      this.confirm();
     },
     savePaymentDetails(data) {
       console.debug('savePaymentDetails/data', data); //DELETE
+      this.confirm();
     },
     saveContract(data) {
       console.debug('saveContract/data', data); //DELETE
+      this.confirm();
     },
   },
   created() { },
