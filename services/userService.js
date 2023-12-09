f1ab1c97d3e0cef2aa1d3369a0bb8b10604b1b04
itWelt <rassulovmurad@gmail.com>
2023-12-09 19:16:06 +0300

    [main][]

--- services/userService.js
+++ services/userService.js
@@ -15,3 +15,18 @@ export const updateUserPersonal = async (data = null) => {
     alert('Ошибка сохранения пользователя'); //FIXME implement with vuetify
   }
 }
+
+export const updateUser = async (data = null) => {
+  console.debug('updateUser/data', data); //DELETE
+  if (!data) return;
+
+  const usersUuidUpdateResponse = await usersUuidUpdate(
+    await userUuidOutAdapter(data)
+  );
+
+  console.debug('usersUuidUpdateResponse', usersUuidUpdateResponse); //DELETE
+
+  if (usersUuidUpdateResponse.status !== 200) {
+    alert('Ошибка обновления данных пользователя'); //FIXME implement with vuetify
+  }
+}
